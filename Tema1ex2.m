60da24968120803a5af6508687dfc1dded02ce28
StancuLuca <32980427+StancuLuca@users.noreply.github.com>
2017-11-08 16:25:23 +0200

    Add files via upload

---
+++ Tema1ex2.m
@@ -0,0 +1,18 @@
+function Tema1ex2() 
+
+ 
+fs = 2000; 
+t = 0:1/fs:100; 
+x = sawtooth(0.4*pi*t, 0.5); %semnal triunghiular 
+                              %perioada 5s  
+plot(t,x); 
+ 
+ 
+ 
+ 
+xlabel('Time (sec)') 
+ylabel('Amplitude') 
+title('Triangular') 
+
+ 
+end 
